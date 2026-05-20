4e9e0788551d6d51fdb412a4dd5ca9e36a3a300d
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2026-05-20 16:12:49 +0300

    chore: remove falco module

--- falco/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-diploma-shared"
-    storage_account_name = "stdiplomarabotastate26"
-    container_name       = "tfstate"
-    key                  = "falco.tfstate"
-  }
-}
